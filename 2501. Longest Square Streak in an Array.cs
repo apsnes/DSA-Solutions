39d7089532611e3a165ca661428358acbb7a7c98
apsnes <161025412+apsnes@users.noreply.github.com>
2024-10-28 09:08:08 +0000

    Create 2501. Longest Square Streak in an Array.cs

---
+++ 2501. Longest Square Streak in an Array.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int LongestSquareStreak(int[] nums)
+    {
+        int longest = 0;
+        SortedSet<int> set = new SortedSet<int>(nums);
+        foreach(int num in nums)
+        {
+            int length = 0;
+            long currNum = num;
+            while (currNum <= int.MaxValue && set.Contains((int)currNum))
+            {
+                length++;
+                currNum = currNum * currNum;
+            }
+            if (length > 1)
+            {
+                longest = Math.Max(longest, length);
+            }
+        }
+        return longest > 1 ? longest : -1;
+    }
+}
