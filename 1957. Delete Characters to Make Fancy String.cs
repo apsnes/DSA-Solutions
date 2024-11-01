97a7986cce4df905603da576346a0295af1650c8
apsnes <161025412+apsnes@users.noreply.github.com>
2024-11-01 09:29:20 +0000

    Create 1957. Delete Characters to Make Fancy String.cs

---
+++ 1957. Delete Characters to Make Fancy String.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public string MakeFancyString(string s)
+    {
+        if (s.Length < 3) return s;
+        
+        StringBuilder sb = new();
+        sb.Append(s[0]);
+        sb.Append(s[1]);
+        for (int i = 2; i < s.Length; i++)
+        {
+            if (s[i] == s[i - 2] && s[i] == s[i - 1])
+            {
+                continue;
+            }
+            sb.Append(s[i]);
+        }
+        return sb.ToString();
+    }
+}
