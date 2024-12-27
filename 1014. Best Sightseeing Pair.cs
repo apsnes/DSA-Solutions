d0c06b375573607b1b8f8937cd901adbd846601b
Joel <161025412+apsnes@users.noreply.github.com>
2024-12-27 13:20:17 +0000

    Update comment explanation to be more detailed

--- 1014. Best Sightseeing Pair.cs
+++ 1014. Best Sightseeing Pair.cs
@@ -1,18 +1,28 @@
-public class Solution {
+public class Solution
+{
     public int MaxScoreSightseeingPair(int[] values)
     {
-        //Initialise a maxLeftScore variable to keep track of the max score to the left of each index
+        //Initialise a variable to keep track of the best left score we've seen so far. We don't care about previous
+        //best scores. If we did, we'd use a dp int[]
         int maxLeftScore = values[0];
         //Initialise a variable to keep track of the highest score we've seen throughout iterations of the algorithm
         int maxScore = 0;
+        //Iterate from 1 to n to check all remaining indicies' scores
         for (int i = 1; i < values.Length; i++)
         {
-            //Calculate the current value of values[i] being used as both the left number (i) and the right number (j)
+            //Intuition tells us that each value in the array can be used as either the left side of the equation 
+            //(values[i] + i) or the right side of the equation  (values[i] - i), so we calculate both:
             int currRight = values[i] - i;
             int currLeft = values[i] + i;
-            //Update maxScore if we have found a higher score
+            //We can use the previously seen maxLeftScore to calculate the current score using currRight:
+            //MaxLeftScore + currRight
+            //Under the hood, that looks like:
+            //(values[i] + i) + (values[j] - j)
+            //Which when rewritten is the same as the problem definition:
+            //Values[i] + values[j] + i - j
+            //So we update maxScore if the new score is higher:
             maxScore = Math.Max(maxScore, maxLeftScore + currRight);
-            //Update maxLeftScore if we have found a higher score
+            //We then update maxLeftScore if the current left score is higher:
             maxLeftScore = Math.Max(maxLeftScore, currLeft);
         }
         return maxScore;
