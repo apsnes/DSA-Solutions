f239d7025f71bd030d614b6afddbcbea067f5963
Joel <161025412+apsnes@users.noreply.github.com>
2024-12-31 14:37:57 +0000

    Update 983. Minimum Cost For Tickets.cs

--- 983. Minimum Cost For Tickets.cs
+++ 983. Minimum Cost For Tickets.cs
@@ -3,7 +3,7 @@ public class Solution
     private int[] _days;
     private int[] _costs;
     private int[] _dp;
-    private HashSet<int> isTravelNeeded;
+    private HashSet<int> _isTravelNeeded;
     public int MincostTickets(int[] days, int[] costs)
     {
         _days = days;
@@ -13,7 +13,7 @@ public class Solution
         //Fill dp array with -1 to indicate values that have not yet been computed
         Array.Fill(_dp, -1);
         //Create a HashSet of days required to travel in order to check if travel is required on a specific day in O(1) time
-        isTravelNeeded = new(days);
+        _isTravelNeeded = new(days);
         return Solve(_days[0]);
     }
     private int Solve(int currDay)
@@ -23,7 +23,7 @@ public class Solution
         //If we've already computed the result for this days minimum cost, return it
         if (_dp[currDay] != -1) return _dp[currDay];
         //If we don't need to travel on the current day, skip it and move to the next day
-        if (!isTravelNeeded.Contains(currDay)) return Solve(currDay + 1);
+        if (!_isTravelNeeded.Contains(currDay)) return Solve(currDay + 1);
         //Calculate the minimum cost recursively for each day
         int result = Math.Min(_costs[0] + Solve(currDay + 1), _costs[1] + Solve(currDay + 7));
         result = Math.Min(result, _costs[2] + Solve(currDay + 30));
