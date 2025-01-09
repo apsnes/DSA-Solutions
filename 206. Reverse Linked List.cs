ec923b4ce092874f6abc39bf99ab764a822f60f4
Joel <161025412+apsnes@users.noreply.github.com>
2025-01-09 08:24:38 +0000

    Recursive solution for 206. Reverse Linked List.cs

--- 206. Reverse Linked List.cs
+++ 206. Reverse Linked List.cs
@@ -1,4 +1,28 @@
-public class Solution {
+//Recursive solution:
+public class Solution
+{
+    public ListNode ReverseList(ListNode head)
+    {   
+        //If we're at the end of the list, return null
+        if (head == null) return null;
+        //Keep track of the last listnode we ever see which will be the new head of the list after reversal
+        ListNode newHead = head;
+        //If theres anything to reverse
+        if (head.next != null)
+        {
+            //Update newHead
+            newHead = ReverseList(head.next);
+            //Set the next.next value to the current value to complete the reversal
+            head.next.next = head;
+        }
+        head.next = null;
+        return newHead;
+    }
+}
+
+//Iterative solution
+public class Solution
+{
     public ListNode ReverseList(ListNode head)
     {
         ListNode prev = null;
