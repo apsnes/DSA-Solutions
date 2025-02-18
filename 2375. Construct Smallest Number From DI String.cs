475c090b7e6cbfc02b18f130285e104c1999afcc
Joel <161025412+apsnes@users.noreply.github.com>
2025-02-18 19:05:03 +0000

    Create 2375. Construct Smallest Number From DI String.cs

---
+++ 2375. Construct Smallest Number From DI String.cs
@@ -0,0 +1,20 @@
+public class Solution
+{
+    public string SmallestNumber(string pattern)
+    {
+        StringBuilder sb = new();
+        Stack<int> stack = new();
+        for (int i = 0; i <= pattern.Length; i++)
+        {
+            stack.Push(i + 1);
+            if (i == pattern.Length || pattern[i] == 'I')
+            {
+                while(stack.Count > 0)
+                {
+                    sb.Append(stack.Pop());
+                }
+            }
+        }
+        return sb.ToString();
+    }
+}
