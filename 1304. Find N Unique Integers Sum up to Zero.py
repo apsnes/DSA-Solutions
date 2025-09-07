288270ab371c8b53147342e7f0b2f04632873f3d
Joel <161025412+apsnes@users.noreply.github.com>
2025-09-07 22:02:17 +0100

    Implement sumZero function to find unique integers

---
+++ 1304. Find N Unique Integers Sum up to Zero.py
@@ -0,0 +1,11 @@
+class Solution:
+    def sumZero(self, n: int) -> List[int]:
+        res = []
+        if n % 2 == 1:
+            res.append(0)
+        i = 1
+        while len(res) < n:
+            res.append(i)
+            res.append(-i)
+            i += 1
+        return res
