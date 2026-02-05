d923949c80da5fbe9c90cd564a7d8ad4d1c39583
Joel <161025412+apsnes@users.noreply.github.com>
2026-02-05 09:27:03 +0000

    Implement ConstructTransformedArray method

---
+++ 3379. Transformed Array.cs
@@ -0,0 +1,15 @@
+public class Solution
+{
+    public int[] ConstructTransformedArray(int[] nums)
+    {
+        var n = nums.Length;
+        var res = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            res[i] = nums[((i + nums[i]) % n + n) % n];
+        }
+
+        return res;
+    }
+}
