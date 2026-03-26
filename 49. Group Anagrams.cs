72c210073f9d66eeba2209ece57077c2477e304a
Joel <161025412+apsnes@users.noreply.github.com>
2026-03-26 21:14:46 +0000

    Add LINQ solution to 49. Group Anagrams.cs

--- 49. Group Anagrams.cs
+++ 49. Group Anagrams.cs
@@ -1,3 +1,4 @@
+// Dictionary Solution
 public class Solution
 {
     public IList<IList<string>> GroupAnagrams(string[] strs)
@@ -19,4 +20,35 @@ public class Solution
         }
         return ans;
     }
+}
+
+// LINQ Solution
+public class Solution
+{
+    public IList<IList<string>> GroupAnagrams(string[] strs)
+    {
+        var stringList = new List<StringObject>();
+
+        for (int i = 0; i < strs.Length; i++)
+        {
+            stringList.Add(new StringObject() { Content = strs[i], OrderedContent = string.Concat(strs[i].OrderBy(c => c)), Index = i});
+        }
+
+        var groupByOrderedString = stringList.GroupBy(str => str.OrderedContent);
+        var res = new List<IList<string>>();
+
+        foreach (var group in groupByOrderedString)
+        {
+            res.Add(group.OrderBy(x => x.Index).Select(s => s.Content).ToList());
+        }
+
+        return res;
+    }
+}
+
+public class StringObject
+{
+    public string Content { get; set; }
+    public string OrderedContent { get; set; }
+    public int Index { get; set; }
 }
