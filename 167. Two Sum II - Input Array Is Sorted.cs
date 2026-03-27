07607fed3436c0ed245bc769bb5603b26ef43217
Joel <161025412+apsnes@users.noreply.github.com>
2026-03-27 21:09:34 +0000

    Add binary search solution for 167. Two Sum II - Input Array Is Sorted.cs

--- 167. Two Sum II - Input Array Is Sorted.cs
+++ 167. Two Sum II - Input Array Is Sorted.cs
@@ -66,3 +66,28 @@ public class Solution
         return new int[0];
     }
 }
+
+
+// Binary search solution:
+public class Solution
+{
+    public int[] TwoSum(int[] numbers, int target)
+    {
+        var n = numbers.Length;
+
+        for (int i = 0; i < n; i++)
+        {
+            var required = target - numbers[i];
+            var left = i + 1;
+            var right = n - 1;
+            while (left <= right)
+            {
+                var mid = (left + right) / 2;
+                if (numbers[mid] == required) return new int[2] { i + 1, mid + 1 };
+                if (numbers[mid] > required) right = mid - 1;
+                else if (numbers[mid] < required) left = mid + 1;
+            }
+        }
+        return new int[2];
+    }
+}
