fbf30310b4e5cb8763e663aadf18aac77e665112
Joel <161025412+apsnes@users.noreply.github.com>
2026-03-28 14:21:57 +0000

    Update 104. Maximum Depth of Binary Tree.cs

--- 104. Maximum Depth of Binary Tree.cs
+++ 104. Maximum Depth of Binary Tree.cs
@@ -21,3 +21,28 @@ public class Solution {
         return 1 + Math.Max(MaxDepth(root.left), MaxDepth(root.right));
     }
 }
+
+// Clearer DFS solution
+public class Solution
+{
+    public int MaxDepth(TreeNode root)
+    {
+        if (root == null) return 0;
+        return SearchDepth(1, root);
+    }
+
+    private int SearchDepth(int currDepth, TreeNode currNode)
+    {
+        var maxDepth = currDepth;
+
+        if (currNode.left != null)
+        {
+            maxDepth = Math.Max(maxDepth, SearchDepth(currDepth + 1, currNode.left));
+        }
+        if(currNode.right != null)
+        {
+            maxDepth = Math.Max(maxDepth, SearchDepth(currDepth + 1, currNode.right));
+        }
+        return maxDepth;
+    }
+}
