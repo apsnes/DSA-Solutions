119736f59e9ed17bbe823310ebebefb9b32878c0
Joel <161025412+apsnes@users.noreply.github.com>
2026-04-03 12:07:02 +0100

    Update 739. Daily Temperatures.cs

--- 739. Daily Temperatures.cs
+++ 739. Daily Temperatures.cs
@@ -1,57 +1,25 @@
-// Initial
 public class Solution
 {
     public int[] DailyTemperatures(int[] temperatures)
     {
         var n = temperatures.Length;
         var res = new int[n];
+        var stack = new Stack<int[]>();
 
-        for (int i = 0; i < n - 1; i++)
+        for (int i = 0; i < n; i++)
         {
-            var currNum = temperatures[i];
-            var stack = new Stack<int>();
-            var futureIndex = i + 1;
-            stack.Push(temperatures[futureIndex]);
-            var numOfDays = 1;
+            var currTemp = temperatures[i];
 
-            while (stack.Count > 0)
+            while (stack.Count > 0 && currTemp > stack.Peek()[0])
             {
-                var currPop = stack.Pop();
-                if (currPop > currNum)
-                {
-                    res[i] = numOfDays;
-                    break;
-                }
-                futureIndex++;
-                if (futureIndex < n) stack.Push(temperatures[futureIndex]);
-                numOfDays++;
+                var pastIndex = stack.Pop()[1];
+                var dayDifference = i - pastIndex;
+                res[pastIndex] = dayDifference;
             }
+
+            stack.Push(new int[2] { currTemp, i });
         }
 
         return res;
-    }
-}
-
-// Optimized
-public class Solution {
-    public int[] DailyTemperatures(int[] temperatures)
-    {
-        int[] output = new int[temperatures.Length];
-        int currIndex = 0;
-        Stack<int[]> stack = new();
-
-        for (int i = 0; i < temperatures.Length; i++)
-        {
-            int t = temperatures[i];
-            while (stack.Count > 0 && t > stack.Peek()[0])
-            {
-                int[] popped = stack.Pop();
-                output[popped[1]] = (i - popped[1]);
-            }
-            int[] toAdd = new int[2];
-            toAdd[0] = t; toAdd[1] = i;
-            stack.Push(toAdd);
-        }
-        return output;
     }
 }
