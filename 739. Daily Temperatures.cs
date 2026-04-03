a5772eb0b0b8780da297a3ea73e0e8e67b5f2407
Joel <161025412+apsnes@users.noreply.github.com>
2026-04-03 11:42:27 +0100

    Update 739. Daily Temperatures.cs

--- 739. Daily Temperatures.cs
+++ 739. Daily Temperatures.cs
@@ -1,3 +1,38 @@
+// Initial
+public class Solution
+{
+    public int[] DailyTemperatures(int[] temperatures)
+    {
+        var n = temperatures.Length;
+        var res = new int[n];
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            var currNum = temperatures[i];
+            var stack = new Stack<int>();
+            var futureIndex = i + 1;
+            stack.Push(temperatures[futureIndex]);
+            var numOfDays = 1;
+
+            while (stack.Count > 0)
+            {
+                var currPop = stack.Pop();
+                if (currPop > currNum)
+                {
+                    res[i] = numOfDays;
+                    break;
+                }
+                futureIndex++;
+                if (futureIndex < n) stack.Push(temperatures[futureIndex]);
+                numOfDays++;
+            }
+        }
+
+        return res;
+    }
+}
+
+// Optimized
 public class Solution {
     public int[] DailyTemperatures(int[] temperatures)
     {
