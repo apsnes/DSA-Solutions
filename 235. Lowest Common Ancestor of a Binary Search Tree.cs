cad1c0f466debb624d0da363d5bf7a387b977977
Joel <161025412+apsnes@users.noreply.github.com>
2026-04-03 21:40:27 +0100

    Update 235. Lowest Common Ancestor of a Binary Search Tree.cs

--- 235. Lowest Common Ancestor of a Binary Search Tree.cs
+++ 235. Lowest Common Ancestor of a Binary Search Tree.cs
@@ -1,13 +1,35 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     public int val;
- *     public TreeNode left;
- *     public TreeNode right;
- *     public TreeNode(int x) { val = x; }
- * }
- */
+// Naive solution
 
+public class Solution
+{
+    TreeNode leftNode;
+    TreeNode rightNode;
+
+    public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+    {
+        leftNode = p.val < q.val ? p : q;
+        rightNode = p.val > q.val ? p : q;
+        return FindLCA(root);
+    }
+
+    private TreeNode FindLCA(TreeNode root)
+    {
+        var isLeft = root.val == leftNode.val ? true : SearchForTarget(root.left, leftNode);
+        var isRight = root.val == rightNode.val ? true : SearchForTarget(root.right, rightNode);
+        if (isLeft && isRight) return root;
+        if (isLeft) return FindLCA(root.left);
+        return FindLCA(root.right);
+    }
+
+    private bool SearchForTarget(TreeNode root, TreeNode target)
+    {
+        if (root == null) return false;
+        if (root.val == target.val) return true;
+        return SearchForTarget(root.left, target) || SearchForTarget(root.right, target);
+    }
+}
+
+// Simple solution
 public class Solution {
     public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
     {
