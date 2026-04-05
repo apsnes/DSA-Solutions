1e8e00cc1ee7e23826e6deb423d17bbb5633be58
Joel <161025412+apsnes@users.noreply.github.com>
2026-04-05 12:06:06 +0100

    Update 213. House Robber II.cs

--- 213. House Robber II.cs
+++ 213. House Robber II.cs
@@ -24,3 +24,58 @@ public class Solution
         return cache[index][flag];
     }
 }
+
+// Bottom up dp
+public class Solution
+{
+    public int Rob(int[] nums)
+    { 
+        if (nums.Length == 1) return nums[0];
+        var excludeLast = nums[..^1];
+        var excludeFirst = nums[1..];
+        if (excludeLast.Length == 1) return Math.Max(excludeLast[0], excludeFirst[0]);
+        var n = excludeLast.Length;
+        var excludeLastMemo = new int[n];
+        var excludeFirstMemo = new int[n];
+        excludeLastMemo[0] = excludeLast[0];
+        excludeLastMemo[1] = Math.Max(excludeLast[0], excludeLast[1]);
+        excludeFirstMemo[0] = excludeFirst[0];
+        excludeFirstMemo[1] = Math.Max(excludeFirst[0], excludeFirst[1]);
+
+        for (int i = 2; i < n; i++)
+        {
+            excludeLastMemo[i] = Math.Max(excludeLastMemo[i - 1], excludeLastMemo[i - 2] + excludeLast[i]);
+            excludeFirstMemo[i] = Math.Max(excludeFirstMemo[i - 1], excludeFirstMemo[i - 2] + excludeFirst[i]);
+        }
+
+        return Math.Max(excludeLastMemo[^1], excludeFirstMemo[^1]);
+    }
+}
+
+// Optimized bottom up DP 
+public class Solution
+{
+    public int Rob(int[] nums)
+    { 
+        if (nums.Length == 1) return nums[0];
+        var excludeLast = nums[..^1];
+        var excludeFirst = nums[1..];
+        if (excludeLast.Length == 1) return Math.Max(excludeLast[0], excludeFirst[0]);
+        return Math.Max(CalculateArrayValue(excludeLast), CalculateArrayValue(excludeFirst));
+    }
+
+    private int CalculateArrayValue(int[] nums)
+    {
+        int prev2 = nums[0];
+        int prev1 = Math.Max(nums[1], nums[0]);
+        
+        for (int i = 2; i < nums.Length; i++)
+        {
+            var curr = Math.Max(prev1, prev2 + nums[i]);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
+    }
+}
