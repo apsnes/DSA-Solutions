1da602c30f9734e1f9a3f3b9b2788df8ac539b62
Joel <161025412+apsnes@users.noreply.github.com>
2026-04-06 10:24:01 +0100

    Update 102. Binary Tree Level Order Traversal.cs

--- 102. Binary Tree Level Order Traversal.cs
+++ 102. Binary Tree Level Order Traversal.cs
@@ -39,3 +39,43 @@ public class Solution {
         return ans;
     }
 }
+
+
+// Tuple
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+ 
+public class Solution
+{
+    public IList<IList<int>> LevelOrder(TreeNode root)
+    {
+        List<IList<int>> res = new();
+        if (root == null) return res;
+
+        var q = new Queue<(TreeNode, int)>();
+        q.Enqueue((root, 0));
+
+        while(q.Count > 0)
+        {
+            (TreeNode node, int level) curr = q.Dequeue();
+            if (res.Count <= curr.level) res.Add(new List<int>());
+            res[curr.level].Add(curr.node.val);
+            if (curr.node.left != null) q.Enqueue((curr.node.left, curr.level + 1));
+            if (curr.node.right != null) q.Enqueue((curr.node.right, curr.level + 1));
+        }
+
+        return res;
+    }
+}
