f632351739c5a8204b7bf77b41636923a7f9556b
Joel <161025412+apsnes@users.noreply.github.com>
2026-04-15 20:25:54 +0100

    Update 78. Subsets.cs

--- 78. Subsets.cs
+++ 78. Subsets.cs
@@ -1,22 +1,30 @@
-public class Solution {
-    public IList<IList<int>> Subsets(int[] nums)
-    {
-        var results = new List<IList<int>>();
-        var subset = new List<int>();
-        Dfs(nums, 0, subset, results);
-        return results;    
-    }
+class Solution
+{
+    private List<List<int>> _subsets = new();
+    private int[] _nums;
 
-    private void Dfs(int[] nums, int i, List<int> subset, List<IList<int>> results)
+	public List<List<int>> Subsets(int[] nums)
+	{
+        _nums = nums;
+
+        var currSet = new List<int>();
+        BuildSubset(currSet, 0);			
+
+		return _subsets.ToList();
+	}
+
+    private void BuildSubset(List<int> currSubset, int currIndex)
     {
-        if (i >= nums.Length)
+        if (currIndex == _nums.Length)
         {
-            results.Add(new List<int>(subset));
+            var copy = new List<int>(currSubset);
+            _subsets.Add(copy);
             return;
         }
-        subset.Add(nums[i]);
-        Dfs(nums, i + 1, subset, results);
-        subset.RemoveAt(subset.Count - 1);
-        Dfs(nums, i + 1, subset, results);
+        
+        currSubset.Add(_nums[currIndex]);
+        BuildSubset(currSubset, currIndex + 1);
+        currSubset.Remove(_nums[currIndex]);
+        BuildSubset(currSubset, currIndex + 1);
     }
 }
