c1e3587e0c03a55788652320f1d2e7e521eb532a
Joel <161025412+apsnes@users.noreply.github.com>
2026-05-19 08:14:15 +0100

    Update 2540. Minimum Common Value.cs

--- 2540. Minimum Common Value.cs
+++ 2540. Minimum Common Value.cs
@@ -1,3 +1,4 @@
+// HashSet
 public class Solution 
     {
     public int GetCommon(int[] nums1, int[] nums2) 
@@ -6,6 +7,23 @@ public class Solution
         foreach (var num in nums1)
         {
             if (set.Contains(num)) return num;
+        }
+        return -1;
+    }
+}
+
+// Two pointer
+public class Solution
+{
+    public int GetCommon(int[] nums1, int[] nums2)
+    {
+        int i = 0, j = 0;
+
+        while (i < nums1.Length && j < nums2.Length)
+        {
+            if (nums1[i] == nums2[j]) return nums1[i];
+            if (nums1[i] < nums2[j]) i++;
+            else j++;
         }
         return -1;
     }
