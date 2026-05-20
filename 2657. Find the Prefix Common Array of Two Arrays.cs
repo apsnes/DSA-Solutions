4a83c72d973404c63e4ed9719160f5bf565fd931
Joel <161025412+apsnes@users.noreply.github.com>
2026-05-20 01:01:12 -0400

    Update 2657. Find the Prefix Common Array of Two Arrays.cs

--- 2657. Find the Prefix Common Array of Two Arrays.cs
+++ 2657. Find the Prefix Common Array of Two Arrays.cs
@@ -24,3 +24,28 @@ public class Solution
         return ans;
     }
 }
+
+// Single Dictionary
+public class Solution
+{
+    public int[] FindThePrefixCommonArray(int[] a, int[] b)
+    {
+        var n = a.Length;
+        var res = new int[n];
+        var currCount = 0;
+        var dict = new Dictionary<int, int>();
+        for (int i = 0; i < n; i++)
+        {
+            var currA = a[i];
+            var currB = b[i];
+            if (!dict.ContainsKey(currA)) dict[currA] = 0;
+            if (!dict.ContainsKey(currB)) dict[currB] = 0;
+            dict[currA]++;
+            dict[currB]++;
+            if (dict[currA] == 2) currCount++;
+            if (currA != currB && dict[currB] == 2) currCount++;
+            res[i] = currCount;
+        }
+        return res;
+    }
+}
