94c31fad0b5920ac695cd94de369842852f01bbf
Joel <161025412+apsnes@users.noreply.github.com>
2026-05-26 08:45:29 +0100

    Update 3120. Count the Number of Special Characters I.cs

--- 3120. Count the Number of Special Characters I.cs
+++ 3120. Count the Number of Special Characters I.cs
@@ -15,3 +15,30 @@ public class Solution
         return count;
     }
 }
+
+
+public class Solution
+{
+    public int NumberOfSpecialChars(string word)
+    {
+        var set = new HashSet<char>(word);
+        var count = 0;
+
+        foreach (char c in word)
+        {
+            if (char.IsLower(c) && set.Contains(char.ToUpper(c)))
+            {
+                count++;
+                set.Remove(c);
+                set.Remove(char.ToUpper(c));
+            }
+            if (char.IsUpper(c) && set.Contains(char.ToLower(c)))
+            {
+                count++;
+                set.Remove(c);
+                set.Remove(char.ToLower(c));
+            }        
+        }
+        return count;
+    }
+}
