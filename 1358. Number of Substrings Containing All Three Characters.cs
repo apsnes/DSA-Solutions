6373ab33fdec58e5e7a97f2b1981b5beb65986f7
Joel <161025412+apsnes@users.noreply.github.com>
2026-06-30 10:14:31 +0100

    Update 1358. Number of Substrings Containing All Three Characters.cs

--- 1358. Number of Substrings Containing All Three Characters.cs
+++ 1358. Number of Substrings Containing All Three Characters.cs
@@ -28,3 +28,35 @@ public class Solution
         return freq.ContainsKey('a') && freq.ContainsKey('b') && freq.ContainsKey('c');
     }
 }
+
+
+
+public class Solution
+{
+    public int NumberOfSubstrings(string s)
+    {
+        if (s.Length == 1) return 0;
+        int a = 0, b = 0, c = 0;
+        int left = 0, right = 0;
+        var res = 0;
+
+        while (right < s.Length)
+        {
+            if (s[right] == 'a') a++;
+            if (s[right] == 'b') b++;
+            if (s[right] == 'c') c++;
+
+            while (a > 0 && b > 0 && c > 0)
+            {
+                res += s.Length - right;
+                if (s[left] == 'a') a--;
+                if (s[left] == 'b') b--;
+                if (s[left] == 'c') c--;
+                left++;
+            }
+            right++;
+        }
+
+        return res;
+    }
+}
